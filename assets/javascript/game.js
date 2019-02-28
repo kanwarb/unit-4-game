65f4d43e9b00865ae5d2ff120924caca1b65bdcf
KB Total Recall <kanwar@gmail.com>
2019-02-27 22:26:35 -0800

    Add New background image, Update Animation to show win or lose

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+
 var minCN =19;
 var maxCN =120;
 var computerNumber = Math.floor(Math.random()*(maxCN - minCN+1)+ minCN); 
@@ -17,7 +18,7 @@ function addImageCrystalToDiv(){
     for(i=0; i < 4; i++){
         imageValue[i] = crystalNumber();
         var imageCrystal = $("<img>");  // add crystal image to the div 
-        imageCrystal.addClass("img-thumbnail col-sm-3 col-xs-12  mt-5");
+        imageCrystal.addClass("img-rounded col-sm-3 col-xs-12  mt-4");
         imageCrystal.attr("id", "crystal-" +i);
         imageCrystal.attr("src", myCrystalsImages[i] );
         imageCrystal.attr( "data-crystal-value", crystalNumber());
@@ -27,23 +28,23 @@ function addImageCrystalToDiv(){
 }
 
 function updateImageCrystalValue() {
-    $('.img-thumbnail').each(function(i) {
+    $('.img-rounded').each(function(i) {
         $(this).attr('data-crystal-value', crystalNumber());
     })
 }    
 
 function playGame(){
-    $(".img-thumbnail").on("click", function(e) {
+    $(".img-rounded").on("click", function(e) {
         var imageValue = ($(this).attr("data-crystal-value"));
         imageValue = parseInt(imageValue);
-        //$.confetti.stop();
         addmySelectedImageValues(imageValue);
         
      })
 }
 
+
+
 function addmySelectedImageValues(imageValue){
-   
     
     console.log( imageValue);
     myPoints += imageValue;
@@ -52,10 +53,11 @@ function addmySelectedImageValues(imageValue){
     {
         myWins++;
         $("#myWinsId").text(" " + myWins)  ;
-        // resetValues();
         animateCSS("W");
-        showStatus("bounce");
-       // clearTimeout(startConfetti);
+        showStatus("jackInTheBox");
+        IntervalId = setTimeout(function() {
+            showStatus("jackInTheBox");
+        }, 8000);
         resetValues();
     }
     else if(myPoints > computerNumber){
@@ -63,22 +65,15 @@ function addmySelectedImageValues(imageValue){
         myLosses++;
         $("#myLossesId").text(" " + myLosses);
         animateCSS("L");
-        showStatus("zoomIn");
-        // setTimeout( startConfetti, 5000);
-        // setTimeout( stopConfetti);
+        showStatus("slideInDown");
+        IntervalId = setTimeout(function() {
+            showStatus("slideInDown");
+        }, 8000);
         resetValues();
     }    
 
 }
 
-function startConfetti() {
-    $.confetti.start();
-}
-
-function stopConfetti() {
-    $.confetti.stop();
-    $.confetti.clear();
-}
 
 function resetValues(){
       console.clear();
@@ -92,52 +87,49 @@ function resetValues(){
       crystalNumber();
       updateImageCrystalValue();
       $("#playerTotalId").text(computerNumber);
-      $("#myScore").text(0);
-      // setTimeout(function () {confetti.stop()}, 3000);
-      
+      $("#myScore").text('');
+      clearInterval(IntervalId);
+      $("#animateH2Id").attr("style", "opacity:1");
+
      
 }
 
 
 function showStatus(x) {
-    
-    $('#animateCSSId').removeClass().addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    $('#animateH2Id').removeClass().addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
       $(this).removeClass();
+      $("#animateH2Id").attr("style", "opacity:0");
     });
-    //var options =  { to: { display: hidden } };
-    //$( "#animateCSSId" ).hide( '.js-animation', {display: none}, 1000, callback );
+
   };
 
 function animateCSS(myResult){
 
-        var animateWin = $("#animateCSSId");
-        var myResponse='';
-        var animateCSS = $("<div>");
-        animateWin.addClass("bounce animated");
-        animateWin.attr("display", "show")
-        if(myResult == "W")
-        {
-         animateWin.addClass("bg-success");
-         myResponse = "You Won ! ";
-        } 
-        else if(myResult =="L")
-        {
-            animateWin.addClass("bg-warning");
-            myResponse = "You Lose This round";
-        }
-      //  animateWin.attr("display" , "block" );
- 
-        $("h2").text(myResponse);
+            var animateWin = $("#animateCSSId");
+            var myResponse='';
+            animateWin.attr("display", "block")
+            if(myResult == "W")
+            {
+             myResponse = "You Won ! ";
+            } 
+            else if(myResult =="L")
+            {
+                myResponse = "You Lose This round";
+            }
+            $("#animateH2Id").attr("font-size", "150%");
+            $("#animateH2Id").text(myResponse);
+                    
         
 
-        $('.js--animation').change(function(){
-            var anim = $(this).val();
-            showStatus("zoomIn");
-          });
+        // $('.js--animation').change(function(){
+        //     var anim = $(this).val();
+        //     showStatus("zoomIn");
+        //   });
     
 
     
     }
+
 
 $("#playerTotalId").text(computerNumber)
 addImageCrystalToDiv();
